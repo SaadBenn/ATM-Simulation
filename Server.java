7dc52808b8fc1e7e3cc26a4a924624463b4147d9
Saad <saadbennmushtaq@gmail.com>
2019-01-29 01:14:56 -0600

    Fix Server code where the messages were not passed correctly

--- Server.java
+++ Server.java
@@ -1,5 +1,4 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,13 +14,13 @@ public class Server {
         InetAddress addr;
         Socket clientSocket = null;
 
-        DataInputStream inputStream = null;
-        DataOutputStream outputStream = null;
-        String command = "E";
+        BufferedReader strm = null;
+        OutputStream outputStream = null;
+        String command = "";
 
         System.out.println("Server starting...");
 
-
+        // Create Socket
         try {
             addr = InetAddress.getLocalHost();
             serverSocket = new ServerSocket(13346, 3, addr);
@@ -32,21 +31,18 @@ public class Server {
             System.exit(1);
         }
 
-
         while (true) {
-
+            // Accept Connection
             try {
                 clientSocket = serverSocket.accept();
-
             } catch (Exception ex) {
                 System.out.println("Accept failed");
                 System.exit(1);
             }
 
-
             try {
-                inputStream = new DataInputStream(clientSocket.getInputStream());
-
+                strm = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                outputStream = clientSocket.getOutputStream();
             } catch (Exception ex) {
                 System.out.println("Socket input stream failed");
                 System.exit(1);
@@ -54,68 +50,146 @@ public class Server {
 
             // Read integers from the connection and print them until
             // a zero is received
+            do {
+                try {
+                    command = strm.readLine();
+                    command = command.trim().replaceAll("\n", "");
+
+                    if (command != null && command != "" && !command.equals("E")) {
+                        processCommand(command, outputStream);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Socket input failed.");
+                    System.exit(1);
+                }
+
+                if (command != null && command != "" && !command.equals("E")) {
+                    try {
+                        outputStream.write("\n".getBytes());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } while (!command.equals("E"));
+
             try {
-                command = inputStream.readUTF();
-                char c = processCommand(command);
-
-                // logic
-
-            } catch (Exception e) {
-                System.out.println("Socket input failed.");
-                System.exit(1);
-            }
-
-
-            // close the stream and sockets
-            try {
-                inputStream.close();
+                outputStream.write("Closing connection".getBytes());
+                strm.close();
                 outputStream.close();
                 clientSocket.close();
-                serverSocket.close();
             } catch (Exception e) {
-                System.out.println("Server couldn't close a socket.");
+                System.out.println("Server couldn't close the socket.");
                 System.exit(1);
             }
         }
+        // close the stream and sockets
+    } // main method
 
-        System.out.println("Server finished.");
-    }
 
-
-    public static char processCommand(String command) {
-        String c;
+    public static void processCommand(String command, OutputStream outputStream) {
         String[] toDo = command.split(",");
+        String clientCommand = toDo[0];
+        String accountNum = toDo[1];
 
-        c = toDo[0];
-        switch (c) {
+        switch (clientCommand) {
             case "C":
-                createAccount(toDo[1]);
+                createAccount(accountNum, outputStream);
+                break;
             case "R":
-                return retrieveAccount(toDo[1]);
+                retrieveAccount(accountNum, outputStream);
+                break;
             case "D":
-                return 'D';
+                processDeposit(accountNum, Integer.parseInt(toDo[2]), outputStream);
+                break;
             case "W":
-                return 'W';
+                processWithdrawl(accountNum, Integer.parseInt(toDo[2]), outputStream);
+                break;
             default:
-                return 'E';
+                try {
+                    outputStream.write("Invalid command".getBytes());
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
         }
-    }
+    } // process command
 
-    public static void createAccount(String account) {
+
+    public static void createAccount(String account, OutputStream outputStream) {
 
         Integer accountNum = Integer.parseInt(account);
-
-        if (!(accounts.containsKey(accountNum))) {
-            accounts.put(accountNum, INITIAL_BALANCE);
+        try {
+            if (!(accounts.containsKey(accountNum))) {
+                accounts.put(accountNum, INITIAL_BALANCE);
+                String output = "Account: " + accountNum + " created successfully with initial balance of 0";
+                outputStream.write(output.getBytes());
+                outputStream.flush();
+            } else {
+                outputStream.write("Account already exists".getBytes());
+                outputStream.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
+    } // create account
 
-    public static Integer retrieveAccount(String account) {
+
+    public static void retrieveAccount(String account, OutputStream outputStream) {
+        Integer accountNum = Integer.parseInt(account);
+        try {
+            if (accounts.containsKey(accountNum)) {
+                String out = "Account retrieved " + "Balance is: " + accounts.get(accountNum);
+                outputStream.write(out.getBytes());
+                outputStream.flush();
+            } else {
+                outputStream.write("Account does not exist".getBytes());
+                outputStream.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } // retrieve account
+
+
+    public static void processDeposit(String account, Integer amount, OutputStream outputStream) {
+        Integer accountNum = Integer.parseInt(account);
+        try {
+            if (accounts.containsKey(accountNum)) {
+                accounts.put(accountNum, accounts.get(accountNum) + amount);
+                String out = "Deposit successful: " + "New balance is: " + accounts.get(accountNum);
+                outputStream.write(out.getBytes());
+                outputStream.flush();
+            } else {
+                outputStream.write("Deposit Failed: Account does not exist.".getBytes());
+                outputStream.flush();
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    } // process deposit
+
+
+    public static void processWithdrawl(String account, Integer amount, OutputStream outputStream) {
         Integer accountNum = Integer.parseInt(account);
 
-        if (accounts.containsKey(accountNum)) {
-            return accounts.get(accountNum);
+        try {
+            if (accounts.containsKey(accountNum)) {
+                if (accounts.get(accountNum) >= amount) {
+                    Integer newBalance = ((accounts.get(accountNum)) - amount);
+                    accounts.put(accountNum, newBalance);
+                    String out = "Withdrawl successful " + "New balance is: " + accounts.get(accountNum);
+                        outputStream.write(out.getBytes());
+                        outputStream.flush();
+                } else {
+                    outputStream.write("Withdrawl Failed: Insufficient balance".getBytes());
+                    outputStream.flush();
+                }
+            } else {
+                outputStream.write("Withdrawl Failed: Account does not exist.".getBytes());
+                outputStream.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return -1;
-    }
-}
+    } // process withdrawl
+} // Server class
